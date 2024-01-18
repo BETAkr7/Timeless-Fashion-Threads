34dca85f6b797c48ba1730ed4fb2cf7e47817de6
AdamRomero17 <156505053+AdamRomero17@users.noreply.github.com>
2024-01-18 19:06:40 +0800

    Add files via upload

---
+++ home.php
@@ -0,0 +1,35 @@
+<html>
+  <head>
+    <title>HOMEPAGE</title>
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+  <body>
+    <header>
+
+      <h2 class="logo">TFT</h2>
+      
+      <nav class="navigation">
+        <a href="about.html">ABOUT</a>
+        <a href="shop.html">SHOP</a>
+        <a href="logout.php"></a>
+        <form action="shop.html" method="post">
+          <button type="submit" class="btnLogin-popup">Login</button>
+        </form>
+      </nav>
+    </header>
+    
+    <main class="row">
+      <div class="col-2">
+        <h1>
+          Give your workout <br />
+          A new style
+        </h1>
+        <p>
+          things you wear to cover your body and keep you warm, <br />
+          to be comfortable, or for the way they make you look <br />
+          I’ve got all these clothes that I never wear.
+        </p>
+      </div>
+    </main>
+  </body>
+</html>
